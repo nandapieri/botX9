37f7f9ec1e7c17b12083b4f03cf6a3a0830368a4
Fernanda Pieri <nandapieri@gmail.com>
2021-11-25 21:56:43 -0300

    aula 3

--- index.js
+++ index.js
@@ -2,26 +2,51 @@
 //max 4314
 //ethbusd
 
-require('dotenv').config()
-const WebSocket = require('ws')
-const ws = new WebSocket('wss://stream.binance.com:9443/ws/ethbusd@kline_1h')
-
-const { Telegraf } = require('telegraf')
-const bot = new Telegraf(process.env.BOT_TOKEN)
-const ethBusdMin = 4129
-const ethBusdMax = 4314
-console.log('conectado')
-
-ws.onmessage = (event) => {
-    const data = JSON.parse(event.data)
-    const valor = parseFloat(data.k.c)
-    console.log('Valor atual:'+data.k.c)
-    if(valor >= ethBusdMax) {
-      bot.telegram.sendMessage(process.env.BOT_CHAT_ID, 'Vender')
-      console.log('Vender')
+function calcRSI(prices) {
+    let ganhos = 0;
+    let perdas = 0;
+    for (let i = prices.length - 14; i < prices.length; i++) {
+        const diferenca = prices[i] - prices[i - 1];
+        if (diferenca >= 0)
+            ganhos += diferenca;
+        else
+            perdas -= diferenca;
     }
-    if (valor <= ethBusdMin) {
-      bot.telegram.sendMessage(process.env.BOT_CHAT_ID, 'Comprar')
-      console.log('Comprar')
-    }
+
+    const forcaRelativa = ganhos / perdas;
+    return 100 - (100 / (1 + forcaRelativa));
 }
+
+(async () => {
+    require('dotenv').config()
+    const axios = require('axios');
+    const candles = await axios.get('https://api.binance.com/api/v3/klines?symbol=ETHBUSD&interval=1m');
+    const closes = candles.data.map(candle => parseFloat(candle[4]));
+
+    const WebSocket = require('ws');
+    const ws = new WebSocket('wss://stream.binance.com:9443/ws/ethbusd@kline_1m');
+
+    const { Telegraf } = require('telegraf');
+    const bot = new Telegraf(process.env.BOT_TOKEN);
+
+    ws.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        if (data.k.x) {
+            closes.push(parseFloat(data.k.c));
+            const rsi = calcRSI(closes);
+            console.log(rsi);
+
+            if (rsi > 70) {
+                bot.telegram.sendMessage(process.env.BOT_CHAT_ID, 'Sobrecomprado!');
+                console.log('Sobrecomprado!');
+            }
+            else if (rsi < 30) {
+                bot.telegram.sendMessage(process.env.BOT_CHAT_ID, 'Sobrevendido!');
+                console.log('Sobrevendido!');
+            }
+        }
+    }
+
+    console.log('conectado');
+
+})()
