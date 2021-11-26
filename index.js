3ea52dfdb423d478dd72eeebdb5fe66d1d57b826
Fernanda Pieri <nandapieri@gmail.com>
2021-11-25 21:51:21 -0300

    aula 2

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 //min 4129
 //max 4314
 //ethbusd
+
+require('dotenv').config()
+const WebSocket = require('ws')
+const ws = new WebSocket('wss://stream.binance.com:9443/ws/ethbusd@kline_1h')
+
+const { Telegraf } = require('telegraf')
+const bot = new Telegraf(process.env.BOT_TOKEN)
 const ethBusdMin = 4129
 const ethBusdMax = 4314
 console.log('conectado')
-const WebSocket = require('ws')
-const ws = new WebSocket('wss://stream.binance.com:9443/ws/ethbusd@kline_1h')
-let vender = false
-let comprar  = false
+
 ws.onmessage = (event) => {
     const data = JSON.parse(event.data)
     const valor = parseFloat(data.k.c)
     console.log('Valor atual:'+data.k.c)
-    if(valor >= ethBusdMax && !vender) {
+    if(valor >= ethBusdMax) {
+      bot.telegram.sendMessage(process.env.BOT_CHAT_ID, 'Vender')
       console.log('Vender')
-      vender = true
-      comprar = false
     }
-    if (valor <= ethBusdMin && !comprar) {
+    if (valor <= ethBusdMin) {
+      bot.telegram.sendMessage(process.env.BOT_CHAT_ID, 'Comprar')
       console.log('Comprar')
-      comprar = true
-      vender = false
     }
 }
